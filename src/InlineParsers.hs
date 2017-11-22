414bf910c00677286e1589f0d2fa81c3f2d60247
jlautman <12954173+jlautman@users.noreply.github.com>
2017-11-22 14:11:46 -0500

    Split strong emphasis from emphasis. Create flanking delimiter parsers

--- src/InlineParsers.hs
+++ src/InlineParsers.hs
@@ -10,14 +10,60 @@ import Text.Parsec.String (Parser)
 import AST
 import ParserCombinators
 
+-- Useful parsers for testing if the next characters match something. None of
+-- these parsers consume input.
+
+-- | Consumes whitespace. Fails if the next character is not without consuming
+--   input.
+whitespace :: Parser Char
+-- Note: Can't use isSpace because Haskell also includes \v. The standard does
+-- not.
+whitespace = try $ oneOf "\t\r\n\f"
+
+-- | Consumes punctuation. Fails if it is not without consuming input.
+punctuation :: Parser Char
+punctuation = try $ oneOf "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+
+-- | Tests if the next data to consume is the provided string and meets the
+--   criteria to be a left flanking delimiter. Consumes the delimiter on
+--   success. Does not consume input on failure.
+leftFlankingDelimiter :: String -> Parser String
+leftFlankingDelimiter s = undefined
+
+-- | Tests if the next data to consume is the provided string and meets the
+--   criteria to be a right flanking delimiter. Consumes the delimiter on
+--   success. Does not consume input on failure.
+rightFlankingDelimiter :: String -> Parser String
+rightFlankingDelimiter s = undefined
+
+-- | Parses inline content and creates an inline AST. Stops at the end of
+--   the block.
+inline :: MdParser
+inline = undefined
+
 code :: MdParser
 code = undefined
 
 italics :: MdParser
 italics = undefined
 
-bold :: MdParser
-bold = undefined
+-- | Parser for an emphasis inline block. Emphasis is signified by another
+--   inline block surrounded by a single * or _ character.
+--   On failure the parser does not consume input.
+--   This parser assumes that the calling parser has already checked that the
+--   left flanking delimiter is not preceded by something to make it invalid.
+--   Does not work with emphasis delimiter _. (Underscore is much more
+--   complicated)
+emphasis :: MdParser
+emphasis = try parseStarEmphasis where
+  parseStarEmphasis :: Parser IlParser
+  parseStarEmphasis = leftFlankingDelimiter "*" *> inline <*
+                      rightFlankingDelimiter "*"
+
+-- | Parser for a strong emphasis inline block. Strong emphasis is signified
+--   by two or three
+strongEmphasis :: MdParser
+strongEmphasis = undefined
 
 link :: MdParser
 link = undefined
