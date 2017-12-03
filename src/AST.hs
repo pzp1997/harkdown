6889088ef27b08c628dec2e679219a05066459c3
jlautman <12954173+jlautman@users.noreply.github.com>
2017-12-03 14:02:16 -0500

    [Arbitrary] Implement arbitrary

--- src/AST.hs
+++ src/AST.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module AST (Markdown(..)) where
+import Test.QuickCheck
+import Control.Applicative
 
 data Markdown
   = Emphasis [Markdown]
@@ -16,3 +20,31 @@ data Markdown
   | InlineLiteral String
   | HorizontalRule
   deriving (Eq, Show)
+
+instance Arbitrary Markdown where
+  arbitrary :: Gen Markdown
+  arbitrary = -- Block level only
+    frequency
+      [ (1, Paragraph <$> genInline) -- Is this correct? Not a list?
+      , (1, OrderedList <$> genInlineList1)
+      , (1, UnorderedList <$> genInlineList1)
+      , (1, BlockQuote <$> genInlineList1)
+      , (1, BlockLiteral <$> arbitrary <*> arbitrary)
+      , (1, InlineLiteral <$> arbitrary)
+      ]
+    where
+    genInline :: Gen Markdown
+    genInline = frequency
+      [ (10, Text <$> arbitrary)
+      , (3, Emphasis <$> genInlineList1)
+      , (3, StrongEmphasis <$> genInlineList1)
+      ]
+    genInlineList :: Gen [Markdown]
+    genInlineList = frequency
+      [ (3, return [])
+      , (4, (:) <$> genInline <*> genInlineList)
+      ]
+    genInlineList1 :: Gen [Markdown]
+    genInlineList1 = liftA2 (:) genInline genInlineList
+  shrink :: Markdown -> [Markdown]
+  shrink m = undefined
