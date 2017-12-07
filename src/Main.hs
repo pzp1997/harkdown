12b8ffce5bdf78b3c17a9130998936319dc79fa2
Palmer Paul <pzpaul2002@yahoo.com>
2017-12-06 20:36:57 -0500

    add basic CLI

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,17 @@
 module Main where
 
+import Control.Monad (unless)
+import System.Environment (getArgs)
+import System.IO (openFile, hGetContents, hClose, IOMode(ReadMode))
+
+import HtmlFormatter (renderHtml)
+import Parser (runMainP)
+
 main :: IO ()
-main = return ()
+main = do
+  args <- getArgs
+  unless (null args) $ do
+    handle <- openFile (head args) ReadMode
+    contents <- hGetContents handle
+    putStrLn $ renderHtml $ runMainP contents
+    hClose handle
