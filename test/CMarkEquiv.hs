2f170dc1a08c298b005ff680603a644fb6805a7a
Palmer Paul <pzpaul2002@yahoo.com>
2017-12-11 23:16:28 -0500

    fix quickcheck

--- test/CMarkEquiv.hs
+++ test/CMarkEquiv.hs
@@ -9,37 +9,37 @@ import AST
 instance Arbitrary Markdown where
   arbitrary = -- Block level only
     frequency
-      [ (1, Paragraph <$> genInline) -- Is this correct? Not a list?
-      , (1, OrderedList <$> genInlineList1)
-      , (1, UnorderedList <$> genInlineList1)
-      , (1, BlockQuote <$> genInlineList1)
+      [ (1, Paragraph <$> genInline)
+      , (1, liftA3 OrderedList arbitrary arbitrary genInlineList1)
+      , (1, liftA2 UnorderedList arbitrary genInlineList1)
+      , (1, (BlockQuote . Many) <$> genInlineList1)
       , (1, CodeBlock <$> arbitrary <*> arbitrary)
-      , (1, InlineLiteral <$> arbitrary)
+      , (1, Code <$> arbitrary)
       ]
     where genInline = frequency [ (10, Text <$> arbitrary)
-                                , (3, Italics <$> genInlineList1)
-                                , (3, Bold <$> genInlineList1)
+                                , (3, (Italics . Many) <$> genInlineList1)
+                                , (3, (Bold . Many) <$> genInlineList1)
                                 ]
           genInlineList = frequency [ (3, return [])
                                     , (4, liftA2 (:) genInline genInlineList)
                                     ]
+          genInlineList1 :: Gen [Markdown]
           genInlineList1 = liftA2 (:) genInline genInlineList
-  shrink (Italics l)       = Italics <$> shrink l
-  shrink (Bold l) = Bold <$> shrink l
-  shrink (Italics l)        = Italics <$> shrink l
+  shrink (Bold l)            = Bold <$> shrink l
+  shrink (Italics l)         = Italics <$> shrink l
   -- For the following two, try both with shrunken link text and without
-  shrink (Link s m)         =
-    [Link s' m' | m' <- shrink m, s' <- shrink s] ++ (Link s <$> shrink m)
-  shrink (Image s m)        =
-    [Link s' m' | m' <- shrink m, s' <- shrink s] ++ (Image s <$> shrink m)
+  shrink (Link s _ m)        =
+    [Link s' Nothing m' | m' <- shrink m, s' <- shrink s]
+  shrink (Image s _ m)       =
+    [Image s' Nothing m' | m' <- shrink m, s' <- shrink s]
   -- For headers, try both with the same header level and with lower
-  shrink (Header i m)       =
+  shrink (Header i m)        =
     [Header i' m' | i' <- shrink i, m' <- shrink m] ++ (Header i <$> shrink m)
-  shrink (Paragraph m)      = Paragraph <$> shrink m
-  shrink (OrderedList l)    = OrderedList <$> shrink l
-  shrink (UnorderedList l)  = UnorderedList <$> shrink l
-  shrink (Text s)           = Text <$> shrink s
-  shrink (BlockQuote l)     = BlockQuote <$> shrink l
-  shrink (CodeBlock m s) = [CodeBlock m' s' | m' <- shrink m, s' <- shrink s]
-  shrink (InlineLiteral s)  = InlineLiteral <$> shrink s
-  shrink HorizontalRule     = []
+  shrink (Paragraph m)       = Paragraph <$> shrink m
+  shrink (OrderedList i b l) = OrderedList i b <$> shrink l
+  shrink (UnorderedList b l) = UnorderedList b <$> shrink l
+  shrink (Text s)            = Text <$> shrink s
+  shrink (BlockQuote l)      = BlockQuote <$> shrink l
+  shrink (CodeBlock m s)     = [CodeBlock m' s' | m' <- shrink m, s' <- shrink s]
+  shrink (Code s)            = Code <$> shrink s
+  shrink _                   = []
